33ed5c61a04a4dbc7e6b91038cf157e097fa3853
Aga <a.kolia.grudzien@gmail.com>
2018-06-07 20:04:25 +0200

    remove ingredients fix

--- burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -42,6 +42,9 @@ addIngredientHandler = (type) => {
 
 removeIngredientHandler = (type) => {
   const oldCount = this.state.ingredients[type];
+  if (oldCount <= 0) {
+    return;
+  }
   const updatedCount = oldCount - 1;
   const updatedIngredients = {
     ...this.state.ingredients
@@ -54,12 +57,19 @@ removeIngredientHandler = (type) => {
 }
 
   render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+      for (let key in disabledInfo) {
+        disabledInfo[key] = disabledInfo[key] <= 0
+      }
     return(
       <Utility>
         <Burger ingredients={this.state.ingredients}/>
         <BuildControls
           ingredientAdded={this.addIngredientHandler}
-          ingredientRemoved={this.removeIngredientHandler}/>
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disabledInfo}/>
       </Utility>
     );
   }
