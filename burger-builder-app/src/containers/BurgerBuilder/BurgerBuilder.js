92d482bee8d431495b390a6b42d2f63bcadb65e6
Aga <a.kolia.grudzien@gmail.com>
2018-06-09 19:32:21 +0200

    display and update the price

--- burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -69,7 +69,8 @@ removeIngredientHandler = (type) => {
         <BuildControls
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
-          disabled={disabledInfo}/>
+          disabled={disabledInfo}
+          price={this.state.totalPrice}/>
       </Utility>
     );
   }
