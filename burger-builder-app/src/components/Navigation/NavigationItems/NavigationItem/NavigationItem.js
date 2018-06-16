ebb48521291153d3335ca5363a255cb22839d58d
Aga <a.kolia.grudzien@gmail.com>
2018-06-16 20:41:17 +0200

    add navigation items

--- burger-builder-app/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ burger-builder-app/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -5,7 +5,10 @@ import classes from './NavigationItem.css';
 
 const navigationItem = (props) => (
   <li className={classes.NavigationItem}>
-    <a href={props.link}>{props.children}</a>
+    <a
+      href={props.link}
+      className={props.active ? classes.active : null}> {props.children}
+    </a>
   </li>
 );
 
