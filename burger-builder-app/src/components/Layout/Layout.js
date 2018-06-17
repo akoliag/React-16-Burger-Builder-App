2037016cb855393811472df50777fa3e180ae409
Aga <a.kolia.grudzien@gmail.com>
2018-06-17 13:07:05 +0200

    reuse Backdrop

--- burger-builder-app/src/components/Layout/Layout.js
+++ burger-builder-app/src/components/Layout/Layout.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, {Component }from 'react';
 import Utility from '../../hoc/Utility';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-  <Utility>
-  <Toolbar />
-  <SideDrawer />
-  <main className={classes.Content}>
-    {props.children}
-  </main>
-  </Utility>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState( { showSideDrawer: false } );
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState( ( prevState ) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        } );
+    }
+
+    render () {
+        return (
+            <Utility>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Utility>
+        )
+    }
+}
+
 
 //higher order component allows us to wrap the content up and return the json
 //elements = we have a wrapping root element - Utility component is the hoc one.
 
-export default layout;
+export default Layout;
