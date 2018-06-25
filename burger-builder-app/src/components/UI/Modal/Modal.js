bb64c2ad17b6a99ed771af9e65998167bbf5efba
Aga <a.kolia.grudzien@gmail.com>
2018-06-25 12:51:59 +0200

    improve performance

--- burger-builder-app/src/components/UI/Modal/Modal.js
+++ burger-builder-app/src/components/UI/Modal/Modal.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './Modal.css';
 import Utility from '../../../hoc/Utility';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-  <Utility>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}>
-      {props.children}
-    </div>
-  </Utility>
-);
+class Modal  extends Component {
+  
+  shouldComponentUpdate(nextProps, nestState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  componentWillUpdate () {
+    console.log('[Modal] WillUpdate')
+  }
+
+  render () {
+    return (
+      <Utility>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div>
+      </Utility>
+    )
+  }
+}
 
 
-
-export default modal;
+export default Modal;
