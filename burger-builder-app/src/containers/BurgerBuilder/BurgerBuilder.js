3cf682360b0a54d0ec02315ae9ec998a64847b55
Aga <a.kolia.grudzien@gmail.com>
2018-06-30 20:04:15 +0200

    add the spinner

--- burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,7 +5,9 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import axios from '../../axios-orders';
+
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -29,7 +31,8 @@ class BurgerBuilder extends Component {
     },
     totalPrice: 4,
     purchasable: false,
-    purchasing: false
+    purchasing: false,
+    loading: false
 }
 
 updatePurchaseState (ingredients) {
@@ -111,14 +114,18 @@ removeIngredientHandler = (type) => {
       for (let key in disabledInfo) {
         disabledInfo[key] = disabledInfo[key] <= 0
       }
+      let orderSummary = <OrderSummary
+          ingredients={this.state.ingredients}
+          price={this.state.totalPrice}
+          purchaseCancelled={this.purchaseCancelHandler}
+          purchaseContinued={this.purchaseContinueHandler}/>
+      if (this.state.loading) {
+          orderSummary = <Spinner />
+      }
     return(
       <Utility>
         <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-          <OrderSummary
-              ingredients={this.state.ingredients}
-              price={this.state.totalPrice}
-              purchaseCancelled={this.purchaseCancelHandler}
-              purchaseContinued={this.purchaseContinueHandler}/>
+          {orderSummary}
         </Modal>
         <Burger ingredients={this.state.ingredients}/>
         <BuildControls
